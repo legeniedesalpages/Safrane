52795c9831a6addabd77c3cc6160b959e48c73a4
BALU Renaud <renaud.balu3@docapost.fr>
2020-01-20 10:46:36 +0100

    Nettoyage + Changement en fullscreen

--- app/src/main/java/fr/lgda/safrane/MainActivity.java
+++ app/src/main/java/fr/lgda/safrane/MainActivity.java
@@ -1,14 +1,22 @@
 package fr.lgda.safrane;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        Log.d("INFO", manager.toString());
+        Log.d("INFO", "- " + manager.getDeviceList().size());
     }
 }
